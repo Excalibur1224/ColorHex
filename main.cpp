6fcd0f8dfebc1000d2d65556f5d89c973087e35a
Excalibur1224 <bafalla@csuchico.edu>
2023-09-20 11:26:03 -0700

    resolved issue #1

--- main.cpp
+++ main.cpp
@@ -7,10 +7,19 @@ int main(){
 
     do{
         std::cout << "Enter a color in hex format (#RRGGBB):";
-        std::getline(cin, input);
+        std::getline(std::cin, input);
 
         if( input.size() != RGB_HEX_LENGTH ){
             std::cout << "Please enter the color in hexadecimal format, starting with # followed by six hex values\n";
+        }
+        else{
+            for(int i=0;i<(int)input.size();++i){
+                if(input[i] > 'f'){
+                    std::cout<<input<<" is not a valid hex value. Please enter valid hex values (0-f)\n";
+                    input = "n";
+                    break;
+                }
+            }
         }
     }while( input.size() != RGB_HEX_LENGTH );
 
